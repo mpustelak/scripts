9a64982b499f22ed82f4743dd1a7652372ee29a9
Mateusz Pustelak <mpustelak@mpustelak.pl>
2017-01-04 09:31:23 +0000

    CloudSearch clear domain PS script

---
+++ aws/CloudSearch/aws-cloudsearch-clear-domain.ps1
@@ -0,0 +1,41 @@
+﻿$searchUrl = '[SEARCH_URL]'
+$documentUrl = '[DOCUMENT_URL]'
+$parser = 'Lucene'
+$querySize = 500
+
+function Get-DomainHits()
+{
+    (Search-CSDDocuments -ServiceUrl $searchUrl -Query "*:*" -QueryParser $parser -Size $querySize).Hits;
+}
+
+function Get-TotalDocuments()
+{
+    (Get-DomainHits).Found
+}
+
+function Delete-Documents()
+{
+    (Get-DomainHits).Hit | ForEach-Object -begin { $batch = '[' } -process { $batch += '{"type":"delete","id":' + $_.id + '},'} -end { $batch = $batch.Remove($batch.Length - 1, 1); $batch += ']' }
+       
+    Try
+    {
+        Invoke-WebRequest -Uri $documentUrl -Method POST -Body $batch -ContentType 'application/json'
+    }
+    Catch
+    {
+        $_.Exception
+        $_.Exception.Message
+    }
+}
+
+$total = Get-TotalDocuments
+while($total -ne 0)
+{
+    Delete-Documents
+
+    $total = Get-TotalDocuments
+
+    Write-Host 'Documents left:'$total
+    # Sleep for 1 second to give CS time to delete documents
+    sleep 1
+}
